defce20d6e3bddce9f61b09c2c5bee9818617b95
DongzeHe <dxh416@case.edu>
2022-01-05 23:35:17 -0500

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -442,7 +442,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         Some(error) => {
                             match *error.kind() {
                                 // if a deserialize error, we already complained about it
-                                ErrorKind::Deserialize { .. } => {}
+                                ErrorKind::Deserialize { .. }=> {return Err("execution terminated unexpectedly".into())},
                                 // if another type of error, just panic for now
                                 _ => {
                                     panic!("could not quantify rad file.");
@@ -482,7 +482,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         Some(error) => {
                             match *error.kind() {
                                 // if a deserialize error, we already complained about it
-                                ErrorKind::Deserialize { .. } => {}
+                                ErrorKind::Deserialize { .. }=> {return Err("execution terminated unexpectedly".into())},
                                 // if another type of error, just panic for now
                                 _ => {
                                     panic!("could not quantify rad file.");
